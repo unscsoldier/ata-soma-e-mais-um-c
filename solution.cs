d3a8ae5463f5e09204ce5fc99f741e4c4f1912ff
unscsoldier <lucas.azvdo@hotmail.com>
2026-08-17 22:16:05 -0300

    feat: publish Soma e mais um (C#)

---
+++ solution.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+      int num1 = int.Parse(Console.ReadLine());
+      int num2 = int.Parse(Console.ReadLine());
+      int soma = num1 + num2 + 1;
+
+      Console.WriteLine(soma);
+    }
+}
